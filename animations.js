d05f64b6d9e026cd46bd89597dcb061d8365419a
Tyler Felgenhauer <tylerfe@tmctrans.com>
2022-06-28 15:47:29 -0500

    making more dots

--- animations.js
+++ animations.js
@@ -1,7 +1,7 @@
 // create instance of kinet with custom settings
 var kinet = new Kinet({
     acceleration: 0.21,
-    friction: 0.56,
+    friction: 0.60,
     names: ["x", "y"],
 });
 var kinet2 = new Kinet({
@@ -14,12 +14,29 @@ var kinet3 = new Kinet({
     friction: 0.70,
     names: ["x", "y"],
 });
+var kinet4 = new Kinet({
+    acceleration: 0.21,
+    friction: 0.75,
+    names: ["x", "y"],
+});
+var kinet5 = new Kinet({
+    acceleration: 0.21,
+    friction: 0.78,
+    names: ["x", "y"],
+});
+var kinet6 = new Kinet({
+    acceleration: 0.21,
+    friction: 0.80,
+    names: ["x", "y"],
+});
 
 // select circle element
 var circle = document.getElementById('circle');
 var circle2 = document.getElementById('circle2');
 var circle3 = document.getElementById('circle3');
-
+var circle4 = document.getElementById('circle4');
+var circle5 = document.getElementById('circle5');
+var circle6 = document.getElementById('circle6');
 
 // set handler on kinet tick event
 kinet.on('tick', function (instances) {
@@ -30,6 +47,15 @@ kinet2.on('tick', function (instances) {
 });
 kinet3.on('tick', function (instances) {
     circle3.style.transform = `translate3d(${(instances.x.current)}px, ${(instances.y.current)}px, 0) rotateX(${(instances.x.velocity / 2)}deg) rotateY(${(instances.y.velocity / 2)}deg)`;
+});
+kinet4.on('tick', function (instances) {
+    circle4.style.transform = `translate3d(${(instances.x.current)}px, ${(instances.y.current)}px, 0) rotateX(${(instances.x.velocity / 2)}deg) rotateY(${(instances.y.velocity / 2)}deg)`;
+});
+kinet5.on('tick', function (instances) {
+    circle5.style.transform = `translate3d(${(instances.x.current)}px, ${(instances.y.current)}px, 0) rotateX(${(instances.x.velocity / 2)}deg) rotateY(${(instances.y.velocity / 2)}deg)`;
+});
+kinet6.on('tick', function (instances) {
+    circle6.style.transform = `translate3d(${(instances.x.current)}px, ${(instances.y.current)}px, 0) rotateX(${(instances.x.velocity / 2)}deg) rotateY(${(instances.y.velocity / 2)}deg)`;
 });
 
 // call kinet animate method on mousemove
@@ -44,6 +70,18 @@ document.addEventListener('mousemove', function (event) {
 document.addEventListener('mousemove', function (event) {
     kinet3.animate('x', event.clientX - window.innerWidth / 2);
     kinet3.animate('y', event.clientY - window.innerHeight / 2);
+});
+document.addEventListener('mousemove', function (event) {
+    kinet4.animate('x', event.clientX - window.innerWidth / 2);
+    kinet4.animate('y', event.clientY - window.innerHeight / 2);
+});
+document.addEventListener('mousemove', function (event) {
+    kinet5.animate('x', event.clientX - window.innerWidth / 2);
+    kinet5.animate('y', event.clientY - window.innerHeight / 2);
+});
+document.addEventListener('mousemove', function (event) {
+    kinet6.animate('x', event.clientX - window.innerWidth / 2);
+    kinet6.animate('y', event.clientY - window.innerHeight / 2);
 });
 
 
